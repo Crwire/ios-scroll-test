6ef8796b3a51e5221a1debe192daffaa58e196c2
Charles Rice <charles.rice92@gmail.com>
2014-11-04 18:02:05 +0000

    status update

--- scrollTest/ViewController.swift
+++ scrollTest/ViewController.swift
@@ -17,13 +17,18 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         super.viewDidLoad()
         // Do cool stuff
         //Part 1
+        let image2 = UIImage(named: "photo2.png")
         let image = UIImage(named: "photo1.png")
         imageView = UIImageView(image: image)
-        imageView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size:image.size)
+        var theImageSize = CGSizeZero
+        if let image = image {
+            theImageSize = image.size
+        }
+        imageView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size:theImageSize)
         scrollView.addSubview(imageView)
         
         //Part2
-        scrollView.contentSize = image.size
+        scrollView.contentSize = theImageSize
         
         //Part 3
         var doubleTapRecognizer = UITapGestureRecognizer(target: self, action: "scrollViewDoubleTapped:")
@@ -51,6 +56,15 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    //Scroll delegate stuff
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        centerScrollViewContents()
+    }
+    
     //Double  tap to zoom
     func scrollViewDoubleTapped(recognizer: UITapGestureRecognizer){
         //Part 1
@@ -58,6 +72,19 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         
         //Part 2
         var newZoomScale = scrollView.zoomScale * 1.5
+        newZoomScale = min(newZoomScale, scrollView.maximumZoomScale)
+        
+        //Part 3
+        let scrollViewSize = scrollView.bounds.size
+        let w = scrollViewSize.width / newZoomScale
+        let h = scrollViewSize.height / newZoomScale
+        let x = pointInView.x - (w / 2.0)
+        let y = pointInView.y - (h / 2.0)
+        
+        let rectToZoomTo = CGRect(x: x, y: y, width: w, height: h)
+        
+        //Part 4
+        scrollView.zoomToRect(rectToZoomTo, animated: true)
     }
     
     //Fixes UIScrollView autodisplay in top-left
